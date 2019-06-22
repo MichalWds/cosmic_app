29b1b1e0fd8e58af9db872e3d27fa1fa74f2738f
MichalWds <michalwadas@onet.pl>
2019-06-22 22:05:55 +0200

    first commit

---
+++ src/main/java/michalwds/CosmicAppApplication.java
@@ -0,0 +1,13 @@
+package michalwds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmicAppApplication.class, args);
+    }
+
+}
