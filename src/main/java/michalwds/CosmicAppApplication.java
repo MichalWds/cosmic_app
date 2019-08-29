c27836254713b2e92d16f9049c934f698694231a
Michał Wadas <michalwadas@onet.pl>
2019-08-29 23:57:25 +0200

    adding Swagger and javaDocs

--- src/main/java/michalwds/CosmicAppApplication.java
+++ src/main/java/michalwds/CosmicAppApplication.java
@@ -1,5 +1,13 @@
 package michalwds;
 
+/**
+ * @Author Michal Wadas
+ * @SpringBootApplication contains all annotations below:
+ * @Configuration
+ * @EnableAutoConfiguration
+ * @ComponentScan
+ */
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +18,11 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 public class CosmicAppApplication {
 
+
+    /**
+     * Static method which run  SpringApplication's static method : "run"
+     * @param args parameter of running class
+     */
     public static void main(String[] args) {
         SpringApplication.run(CosmicAppApplication.class, args);
     }
--- src/main/java/michalwds/controllers/HomeController.java
+++ src/main/java/michalwds/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package michalwds.view;
+package michalwds.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
