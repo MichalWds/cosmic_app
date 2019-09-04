7d0ada86958017b7c8304538130620857e93ef6c
Michał Wadas <michalwadas@onet.pl>
2019-09-04 21:52:27 +0200

    creating class with method to create excel file with all object

--- src/main/java/michalwds/controllers/HomeController.java
+++ src/main/java/michalwds/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package michalwds.controllers;
 
+import michalwds.extras.CreatorXLS;
 import michalwds.models.dtos.PlanetDTO;
 import michalwds.services.PlanetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 @Controller
 public class HomeController {
@@ -32,16 +36,21 @@ public class HomeController {
     }
 
     @GetMapping("/delete")
-    public String deletePlanet(@RequestParam(value = "planet") String planetName){
+    public String deletePlanet(@RequestParam(value = "planet") String planetName) {
         planetService.deletingPlanet(planetName);
         return "redirect:/planets";
     }
 
     @PostMapping("/add")  //model attribute binds all inputs using keys
-    public String addPlanet(@ModelAttribute PlanetDTO planet){
+    public String addPlanet(@ModelAttribute PlanetDTO planet) {
         planetService.addPlanet(planet);
         return "redirect:/planets";
     }
 
-
+    @GetMapping("/excel")
+    public String createFile() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException {
+        CreatorXLS<PlanetDTO> creatorXLS = new CreatorXLS<>(PlanetDTO.class);
+        creatorXLS.createFile(planetService.getPlanetsDTO(), "src/main/resources", "planets");
+        return "redirect:/index";
+    }
 }
